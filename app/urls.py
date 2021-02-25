92bc9ddc3d608f8e56299cae28f836a766e1a826
rotiroti <rotirotirafa@gmail.com>
2021-02-25 13:19:48 -0300

    new configs

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-"""django_teasty URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+
 ]
